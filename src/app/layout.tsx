15fb4a1404e780d6ee03a9ad15035bfb58491775
Dean Lofts <dean@deanlofts.xyz>
2023-12-27 18:44:43 +1100

    formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,19 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'GalleryGrid',
-  description: 'GalleryGrid: A sleek Next.js app for managing & showcasing images in a dynamic, responsive grid. Perfect for visual storytelling.',
-}
+  description:
+    'GalleryGrid: A sleek Next.js app for managing & showcasing images in a dynamic, responsive grid. Perfect for visual storytelling.',
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
